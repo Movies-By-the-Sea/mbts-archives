5100e027f32918d548a1a65a27b52da02722b084
Saumya Bhatt <saumya.bhatt106@gmail.com>
2020-06-06 22:14:25 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Movies-by-the-Sea
+Personal Movie Review Blog Page
+
+![Website Screenshot](static/images/movies_SS.png/?raw=true "Optional Title")
+
+
---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, url_for, request
+app = Flask('__name__')
+
+import csv
+with open('reviews.csv') as f:
+    reader = csv.DictReader(f)
+    data = [r for r in reader]
+
+import requests
+import json
+
+def userInput(query):
+    url = "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/"+query
+
+    headers = {
+        'x-rapidapi-host': "imdb-internet-movie-database-unofficial.p.rapidapi.com",
+        'x-rapidapi-key' : "42a81c1684msh89e08ede69c15b4p1df539jsn876cffad9666"
+        }
+
+    response = requests.request("GET", url, headers=headers)
+    store = response.json()
+
+    count = 0
+    actors = list()
+    for i in store["cast"]:
+        if count > 5:
+            break
+        actors.append(i['actor'])
+        count += 1
+
+    MovieDetails = {
+        'title' : store['title'],
+        'year'  : store['year'],
+        'plot'  : store['plot'],
+        'actors': actors
+    }
+    return MovieDetails
+
+@app.route('/reviews.html')
+def open():
+    return render_template('reviews.html', data=data)
+
+@app.route('/')
+@app.route('/index.html')
+def home():
+    return render_template('index.html', data=data, result=userInput('Get Out'))
+
+@app.route('/', methods=['POST'])
+@app.route('/index.html', methods=['POST'])
+def my_form_post():
+    text = request.form['searchMovie']
+    processed_text = userInput(text)
+    return render_template('index.html',data=data,result=processed_text)
+
+if __name__ == '__main__':
+    app.run(debug=True)
